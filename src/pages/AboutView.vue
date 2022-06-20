8f1895c2514bdcb8d1abf262e81eab143791c7e0
tuankiet0901 <kiet-lt@genkisystem.com>
2022-06-20 11:07:32 +0700

    Check store

--- src/pages/AboutView.vue
+++ src/pages/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page from FE-Store with Actions in Github</h1>
+    <h2>Profile: {{ store.state.user.userData }}</h2>
   </div>
 </template>
+
+<script>
+import { useStore } from "vuex";
+
+export default {
+  name: "AboutView",
+  setup() {
+    const store = useStore();
+
+    return {
+      store,
+    };
+  },
+};
+</script>
